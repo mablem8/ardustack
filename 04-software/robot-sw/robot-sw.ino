e6961e4390341ea26047b8c0aa0e727b5f906528
Brad Denby <bdenby@cap602.org>
2022-02-06 16:28:54 -0500

    Updates robot software

--- 04-software/robot-sw/robot-sw.ino
+++ 04-software/robot-sw/robot-sw.ino
@@ -10,31 +10,31 @@ void setup() {
   headServo.attach(3);
   tailServo.attach(11);
   delay(1000);
-  headServo.write(90);
+  headServo.write(100);
   delay(1000);
-  tailServo.write(90);
+  tailServo.write(100);
   delay(1000);
 }
 
 void loop() {
-  while(tailServo.read()<95) {
+  while(tailServo.read()<105) {
     tailServo.write(tailServo.read()+1);
-    delay(33);
+    delay(50);
   }
-  delay(100);
-  while(headServo.read()<100) {
+  delay(200);
+  while(headServo.read()<110) {
     headServo.write(headServo.read()+1);
-    delay(33);
+    delay(50);
   }
-  delay(100);
-  while(tailServo.read()>85) {
+  delay(200);
+  while(tailServo.read()>95) {
     tailServo.write(tailServo.read()-1);
-    delay(33);
+    delay(50);
   }
-  delay(100);
-  while(headServo.read()>80) {
+  delay(200);
+  while(headServo.read()>90) {
     headServo.write(headServo.read()-1);
-    delay(33);
+    delay(50);
   }
-  delay(100);
+  delay(200);
 }
